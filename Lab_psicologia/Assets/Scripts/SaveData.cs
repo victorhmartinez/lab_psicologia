a4d2ffdc75691665c2c9c1b017870bf281366796
victorhmartinez <victormartinezg1998@gmail.com>
2024-07-17 17:58:41 -0500

    Probando animacioneds

--- Lab_psicologia/Assets/Scripts/SaveData.cs
+++ Lab_psicologia/Assets/Scripts/SaveData.cs
@@ -6,34 +6,37 @@ using Proyecto26;
 
 public class SaveData : MonoBehaviour
 {
-    private string url_api = "https://api-lab-psicologia.onrender.com/api";
+    private string url_api = "http://localhost:3000/api";
 
     public void writeNewUser(string name, string email, string username, string date)
     {
-        User user = new User(name, email, username,  date);
+        User user = new User(name, email, username, date);
         print(user.username);
-        print(user.nombre);
+        print(user.nombre_completo);
 
-        /*  RestClient.Post(url_api + "create-user", user).Then(
+        RestClient.Post(url_api + "/create-user", user).Then(
               response =>
               {
-                  Debug.Log("usuario registrado con éxito");
+                  Debug.Log("Usuario registrado con éxito");
               }
-          );*/
-
+          ).Catch(err =>
+          {
+              Debug.LogError("Error al registrar el usuario: " + err.Message);
+          }
+          );
     }
 }
 
 class User
 {
-    public string nombre;
+    public string nombre_completo;
     public string email;
     public string username;
     public string date;
 
-    public User(string nombre, string email, string username, string date)
+    public User(string nombre_completo, string email, string username, string date)
     {
-        this.nombre = nombre;
+        this.nombre_completo = nombre_completo;
         this.email = email;
         this.username = username;
         this.date = date;
